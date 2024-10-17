83f1e60f87b7f77602a8fd32dd031f716a863b4e
syedjani3012 <syedjani3012@gmail.com>
2024-10-17 21:34:47 +0530

    adding hooks

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import './App.css';
+import {useState} from 'react';
 
 function App() {
+
+  let [state, setstate] = useState("");
+  const message=()=>{
+    setstate("login succesful")
+  }
+
   return (
     <div id="login">
     <h1> Login with OTP </h1>
     <p>Mobile number: <input></input></p>
-    <button>Login</button>
+    <button onClick={message}> Login</button>
+    <p>{state}</p>
     </div>
   );
 }
