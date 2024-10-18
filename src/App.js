4ada373ec359b9d0a9fa6b4ae6e867049c061132
syedjani3012 <syedjani3012@gmail.com>
2024-10-18 20:52:28 +0530

    new version

--- src/App.js
+++ src/App.js
@@ -1,20 +1,28 @@
-import './App.css';
-import {useState} from 'react';
+import { useState } from 'react'
+import './App.css'
 
 function App() {
 
-  let [state, setstate] = useState("");
-  const message=()=>{
-    setstate("login succesful")
+  let [col,setcol]=useState('white')
+
+  const changecolor=(e)=>{
+    const color=e.target.innerText
+    setcol(color)
+    
   }
 
   return (
-    <div id="login">
-    <h1> Login with OTP </h1>
-    <p>Mobile number: <input></input></p>
-    <button onClick={message}> Login</button>
-    <p>{state}</p>
+
+    <div className="body" style={{backgroundColor: col}}>
+      <h1 className='mb-11'>Background Changer</h1>
+      <h3>click the button and see magic</h3>
+      <div className='but'> 
+        <button className="blue" onClick={changecolor}>Blue</button>
+        <button  className="red" onClick={changecolor}>Red</button>
+        <button className="green" onClick={changecolor}>Green</button>
+      </div>
     </div>
-  );
+  )
 }
-export default App;
+
+export default App
